5a951f0a07856a32a53f58a79b27445d0d4ea223
RedEyedChief <machulyanskiy.dmytro.wsk@gmail.com>
2024-07-05 19:42:32 +0300

    initial commit

---
+++ src/main.ts
@@ -0,0 +1,10 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ThrottlerGuard } from '@nestjs/throttler';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.useGlobalGuards(app.get(ThrottlerGuard));
+  await app.listen(3001);
+}
+bootstrap();
